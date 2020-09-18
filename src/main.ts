14e270faab1b4c73b6f3ed18c7740e7b84bd5323
JunzoMatsunoo <junzo.matsunoo@wireframes.co.jp>
2020-09-18 11:13:29 +0900

    revert version, main.js to ts, directory change

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import * as BaseUi from './index'
+import 'vue/types/vue'
+import { listSvgFileNames } from './theme/helpers/listSvgFileNames'
 
 export default {
-  install(Vue, options) {
+  install(Vue: any, options: any) {
     Object.keys(BaseUi).forEach((key) => {
       Vue.component(key, BaseUi[key])
     })
@@ -10,3 +14,5 @@ export default {
     })
   }
 }
+
+export { listSvgFileNames }
