c83dd2a371b7cb596e3a2ac44f789543be7109f9
JunzoMatsunoo <junzo.matsunoo@wireframes.co.jp>
2020-10-16 07:20:53 +0900

    workspace update.

--- workspace/src/App.vue
+++ workspace/src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <base-box type="white" padding="12px">
-    <div>App.ですよ<base-button>ボタンだよね</base-button></div>
+    <base-box-title>This is Title.</base-box-title>
+    <div>
+      <base-button>button</base-button>
+    </div>
+    <div>
+      <base-button type="secondary" size="large">button secondary large</base-button>
+    </div>
+    <div>
+      <base-button type="border" size="small">border small</base-button>
+    </div>
+    <template v-slot:footer>
+      footer for base-box
+    </template>
   </base-box>
 </template>
 <script>
 import BaseButton from '../../src/vue/components/Button/button.ejs'
+import BaseBox from '../../src/vue/components/Box/box.ejs'
+import BaseBoxTitle from '../../src/vue/components/Box/box-title.ejs'
 export default {
-  components: { BaseButton },
+  components: { BaseButton, BaseBox, BaseBoxTitle },
   name: 'App'
 }
 </script>
--- workspace/src/index.js
+++ workspace/src/index.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import App from './App'
-import test from '../../src/vue/components/Button/button.ejs'
-
-console.log('this is test!!!------', test)
-// Vue.use(UiBase)
 
 new Vue({
   render: (h) => h(App)
